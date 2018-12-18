8ca496c51fd85212c38f9b2409c139727daf313b
zawadka <louis.78100@hotmail.fr>
2018-12-18 09:21:34 +0100

    second push

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,47 @@
+var direction;
+var nextDirection;
+var floor;
+var speed = 100;
+var portalPosition;
+var size = 300;
+var portal;
+var opposites = {
+  // Usefull to calculate an impossible turn
+  left: 'right',
+  up: 'down',
+  right: 'left',
+  down: 'up',
+};
+var portalPosition;
+
+
+/* CLIC PASSAGE AU JEU */
 oxo.inputs.listenKey('enter', function() {
   if (oxo.screens.getCurrentScreen !== 'game') {
     oxo.screens.loadScreen('game');
   }
 });
+/* CLIC PASSAGE AU JEU */
 
+/* Move of obstacle */
+
+
+portal = document.getElementById('portal');
+oxo.animation.move(portal, 'right', speed * (1));
+portalPosition = oxo.animation.getPosition(portal);
+
+oxo.inputs.listenArrowKeys(function(key) {
+
+  if (key !== opposites[direction]) {
+    nextDirection = key;
+  }
+});
+
+direction = nextDirection = 'right';
+  oxo.animation.move(portal, direction, size, true); 
+  portalPosition = oxo.animation.getPosition(portal);
+/* Move of obstacle */
+
+/* move of the character */
+
+/* move of the character */
