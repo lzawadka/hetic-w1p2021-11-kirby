aed8330ccdad430e07e8400781b9959ed39d80b3
zawadka <louis.78100@hotmail.fr>
2018-12-19 22:50:47 +0100

    obstacle placed

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,25 @@
 
-
 var direction;
 var nextDirection;
 var floor;
 var speed = 100;
 var characterPosition;
 var size = 300;
-var gravity = 7;  //Speed of falling
+var gravity = 5;  //Speed of falling
 var time;
 var flying = true;
 var chutInterval;
 var directionDown = 'down';
+var directionUp = 'up'; 
 var character;
 var ground;
 var objInterval;
 var audioJump
 var theme;
 var audioPortal;
+var groundDark = document.querySelector('#ground__dark');
+var darkWorld = false;
+var negative;
 var charPosY = 600;
 
 /* CLIC PASSAGE AU JEU */
@@ -27,6 +30,12 @@ oxo.inputs.listenKeyOnce('enter', function() {
     console.log(true);
   }
 });
+
+/* oxo.inputs.listenKey('space', function() {
+  if (oxo.screens.getCurrentScreen !== 'home') {
+    oxo.screens.loadScreen('home');
+  }
+});*/
 
 //Appel des fonctions
 
@@ -39,34 +48,57 @@ function game() {
   chutInterval = setInterval(player, 7.5);
   character = document.getElementById('container__character');    
   oxo.animation.setPosition(character, {x: 100, y: 600});
+  //darkFloor();
+  //ground = document.getElementsByClassName('ground__dark');
   floor();
-  ground = document.getElementsByClassName('floor');
+  ground0 = document.getElementsByClassName('floor');
   obstacle1();
-  ground2 = document.getElementsByClassName('obstacle1');
+  ground1 = document.getElementsByClassName('obstacle1');
   obstacle2();
-  ground3 = document.getElementsByClassName('obstacle2');
+  ground2 = document.getElementsByClassName('obstacle2');
   obstacle3();
-  ground4 = document.getElementsByClassName('obstacle3');
+  ground3 = document.getElementsByClassName('obstacle3');
   obstacle4();
-  ground5 = document.getElementsByClassName('obstacle4');
+  ground4 = document.getElementsByClassName('obstacle4');
   obstacle5();
-  ground6 = document.getElementsByClassName('obstacle5');
+  ground5 = document.getElementsByClassName('obstacle5');
+  obstacle6();
+  ground6 = document.getElementsByClassName('obstacle6');
+  obstacle7();
+  ground7 = document.getElementsByClassName('obstacle7');
+  obstacle8();
+  ground8 = document.getElementsByClassName('obstacle8');
+  obstacle9();
+  ground9 = document.getElementsByClassName('obstacle9');
 
 //move of the character
 
+
+/*if ( flying === true) {
+  oxo.animation.move(container__character, 'down', 1);
+  };
+
+  oxo.inputs.listenArrowKeys(function(key) {
+    if ( key === 'down' ) {
+      oxo.animation.move(container__character, 'down', 50);
+    } else if (key === 'up' && !flying) {
+      audioJump.play();
+      oxo.animation.move(container__character, 'up', 600 * negative);
+        charPosY -= 900 * negative;
+        flying = true;
+    }
+  });*/
 
 if ( flying === true) {
   oxo.animation.move(container__character, 'down', 3);
 };
 
 oxo.inputs.listenArrowKeys(function(key) {
-  console.log(flying);
-  console.log(`So6: ${oxo.animation.getPosition(character).y}`);
   if ( key === 'down' ) {
     oxo.animation.move(container__character, 'down', 100);
   } else if (key === 'up' && !flying) {
     audioJump.play();
-    oxo.animation.move(container__character, 'up', 700, true);
+    oxo.animation.move(container__character, 'up', 800, true);
       charPosY -= 900;
       flying = true;
   }
@@ -78,7 +110,7 @@ oxo.inputs.listenArrowKeys(function(key) {
 setInterval(
   function() {
     var obstacles = document.querySelector('.obstacle1');
-    oxo.animation.move(obstacles, 'left', 8, true);
+    oxo.animation.move(obstacles, 'left', 9, true);
   }, 
   20      
 );
@@ -89,7 +121,7 @@ setInterval(
   function() {
     var obstacles2 = document.querySelector('.obstacle2');
 
-    oxo.animation.move(obstacles2, 'left', 8, true);
+    oxo.animation.move(obstacles2, 'left', 9, true);
   }, 
   20
 );  
@@ -100,7 +132,7 @@ setInterval(
   function() {
     var obstacles3 = document.querySelector('.obstacle3');
 
-    oxo.animation.move(obstacles3, 'left', 8, true);
+    oxo.animation.move(obstacles3, 'left', 9, true);
   }, 
   20 
 );
@@ -111,7 +143,7 @@ setInterval(
   function() {
     var obstacles4 = document.querySelector('.obstacle4');
 
-    oxo.animation.move(obstacles4, 'left', 8, true);
+    oxo.animation.move(obstacles4, 'left', 9, true);
   }, 
   20 
 );
@@ -122,10 +154,56 @@ setInterval(
   function() {
     var obstacles5 = document.querySelector('.obstacle5');
 
-    oxo.animation.move(obstacles5, 'left', 8, true);
+    oxo.animation.move(obstacles5, 'left', 9, true);
   }, 
   20 
 );
+
+//Fonction obstacle6
+
+setInterval(
+  function() {
+    var obstacles6 = document.querySelector('.obstacle6');
+
+    oxo.animation.move(obstacles6, 'left', 9, true);
+  }, 
+  20 
+);
+
+//Fonction obstacle7
+
+setInterval(
+  function() {
+    var obstacles7 = document.querySelector('.obstacle7');
+
+    oxo.animation.move(obstacles7, 'left', 9, true);
+  }, 
+  20 
+);
+
+//Fonction obstacle8
+
+setInterval(
+  function() {
+    var obstacles8 = document.querySelector('.obstacle8');
+
+    oxo.animation.move(obstacles8, 'left', 9, true);
+  }, 
+  20 
+);
+
+//Fonction obstacle9
+
+setInterval(
+  function() {
+    var obstacles9 = document.querySelector('.obstacle9');
+
+    oxo.animation.move(obstacles9, 'left', 9, true);
+  }, 
+  20 
+);
+
+
 
 //Fonction gravité character
 
@@ -133,7 +211,7 @@ function player() {
   if(charPosY <= 900){
     charPosY += gravity;
   }
-  oxo.animation.move(container__character, directionDown, gravity, true);
+  oxo.animation.move(character, directionDown, gravity, true);
   if(flying){
   if(charPosY >= 900){
       flying = false;
@@ -141,21 +219,48 @@ function player() {
   }
 }
 
+/*function player() {
+  if(charPosY <= 600 && negative === 1){ // Si la position verticale de kirby est inférieur ou égale à 600 && que var negative est égale à 1 
+    charPosY += gravity; // Alors on applique une gravité positive
+  }else if(charPosY >= -600 && negative === -1){
+    charPosY -= gravity; // Sinon on applique une gravité négative
+  }
+  if(negative === 1){
+    oxo.animation.move(container__character, directionDown, gravity, true);
+  }else{
+    oxo.animation.move(container__character, directionUp, gravity, true);
+  }
+  if(flying){
+    if(charPosY >= 600 * negative){
+      flying = false;
+    }
+  }
+} */
+
 //Fonction floor
 function floor() {
-    var floor = oxo.elements.createElement({
-      obstacle: true,
-      class: 'floor',
-      styles: {
-        transform:
-          'translate(100px, 705px)',
-      },
-    });     
+  var floor = oxo.elements.createElement({
+    obstacle: true,
+    class: 'floor',
+    styles: {
+      transform:
+        'translate(100px, 705px)',
+    },
+  });     
  }
 
- 
+function darkFloor () {
+  var darkFloor = oxo.elements.createElements({
+    obstacle : true,
+    class: 'ground__dark',
+    transform:
+    'translate(100px, 0px)',
+  });
+
+}
+
 //obstacle1
- function obstacle1() {
+function obstacle1() {
    var obstacle1 = oxo.elements.createElement({
     obstacle: true,
     class: 'obstacle1', 
@@ -168,8 +273,8 @@ function floor() {
 }
 
 // obstacle2
- function obstacle2() {
-   var obstacle2 = oxo.elements.createElement({
+function obstacle2() {
+  var obstacle2 = oxo.elements.createElement({
     obstacle: true,
     class: 'obstacle2',   
   });     
@@ -183,38 +288,104 @@ function floor() {
 // portal obstacle3
 function obstacle3() {
   var obstacle3 = oxo.elements.createElement({
-   obstacle: true,
-   class: 'obstacle3',   
+    obstacle: true,
+    class: 'obstacle3',   
  });     
  
- oxo.elements.onCollisionWithElement(character, obstacle3, function() {
-  audioPortal.play();
+  oxo.elements.onCollisionWithElement(character, obstacle3, function() {
+    audioPortal.play();
  });
 }
 
 // obstacle4
 function obstacle4() {
   var obstacle4 = oxo.elements.createElement({
-   obstacle: true,
-   class: 'obstacle4',   
+    obstacle: true,
+    class: 'obstacle4',   
  });     
  
- oxo.elements.onCollisionWithElement(character, obstacle4, function() {
-  oxo.screens.loadScreen('end', function() {
-  });
- });
-}
-// obstacle5
-function obstacle5() {
-  var obstacle5 = oxo.elements.createElement({
-   obstacle: true,
-   class: 'obstacle5',   
- });     
- 
- oxo.elements.onCollisionWithElement(character, obstacle5, function() {
-  oxo.screens.loadScreen('end', function() {
+  oxo.elements.onCollisionWithElement(character, obstacle4, function() {
+    oxo.screens.loadScreen('end', function() {
   });
  });
 }
 
+// obstacle5
+function obstacle5() {
+  var obstacle5 = oxo.elements.createElement({
+    obstacle: true,
+    class: 'obstacle5',   
+ });     
+ 
+  oxo.elements.onCollisionWithElement(character, obstacle5, function() {
+    oxo.screens.loadScreen('end', function() {
+  });
+ });
+}
+
+// obstacle6
+function obstacle6() {
+  var obstacle6 = oxo.elements.createElement({
+    obstacle: true,
+    class: 'obstacle6',   
+ });     
+ 
+  oxo.elements.onCollisionWithElement(character, obstacle6, function() {
+    oxo.screens.loadScreen('end', function() {
+  });
+ });
+}
+
+// obstacle7
+function obstacle7() {
+  var obstacle7 = oxo.elements.createElement({
+    obstacle: true,
+    class: 'obstacle7',   
+ });     
+ 
+  oxo.elements.onCollisionWithElement(character, obstacle7, function() {
+    oxo.screens.loadScreen('end', function() {
+  });
+ });
+}
+
+// obstacle8
+function obstacle8() {
+  var obstacle8 = oxo.elements.createElement({
+    obstacle: true,
+    class: 'obstacle8',   
+ });     
+ 
+  oxo.elements.onCollisionWithElement(character, obstacle8, function() {
+    oxo.screens.loadScreen('end', function() {
+  });
+ });
+}
+
+// obstacle9
+function obstacle9() {
+  var obstacle9 = oxo.elements.createElement({
+    obstacle: true,
+    class: 'obstacle9',   
+ });     
+ 
+  oxo.elements.onCollisionWithElement(character, obstacle9, function() {
+    oxo.screens.loadScreen('end', function() {
+  });
+ });
+}
+
+
+//
+/*oxo.elements.onCollisionWithElement(character, obstacle3, function(){
+  if(!darkWorld) {
+    negative = -1;
+    groundDark.classList = "ground__dark--is-collided";
+    darkWorld = true;
+  } else {
+    negative = 1;
+    groundDark.classList = "ground__dark";
+    darkWorld = false; 
+  }
+});*/
 };
